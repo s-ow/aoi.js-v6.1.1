82e4425c644945e27bf2908668868a3fb231e54e
s-ow <107432480+s-ow@users.noreply.github.com>
2023-02-23 23:40:37 +0100

    Update variables.js

--- handler/variables.js
+++ handler/variables.js
@@ -1,4 +1,4 @@
-module.exports=(bot)=>{
+module.exports=(bot)=>{ //wiki aoi.js : https://aoi.js.org/docs/guides/variables
     bot.variables({
         prefix:"=", //n'oubliez pas la virgule à chaque fois que vous ajoutez une nouvelle variable
       variable:"valeur", //exemple rapide
