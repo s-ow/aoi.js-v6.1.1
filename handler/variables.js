169a168eb3ba114e737ce18ca8b499be90c3cc05
s-ow <107432480+s-ow@users.noreply.github.com>
2023-02-23 23:38:14 +0100

    Create variables.js

---
+++ handler/variables.js
@@ -0,0 +1,7 @@
+module.exports=(bot)=>{
+    bot.variables({
+        prefix:"=", //n'oubliez pas la virgule à chaque fois que vous ajoutez une nouvelle variable
+      variable:"valeur", //exemple rapide
+
+    },"main")
+}
