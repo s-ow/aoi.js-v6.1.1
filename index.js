c1643b21566324d5085fea22bfca92b6220fb073
s-ow <107432480+s-ow@users.noreply.github.com>
2023-02-23 23:21:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const aoijs = require('aoi.js'); //aoi.js is required to code in aoi.js
-const config = require('./config.js'); //file where we will put the configuration infos
-const bot = new aoijs.AoiClient(config.Bot); //creation of the object Bot
+//<------Configuration des constantes------>
+const aoijs = require('aoi.js');
+const config = require('./config.js'); //création d'un handler de configuration
+const bot = new aoijs.AoiClient(config.Bot); //création de l'objet Bot
 const {
     AoiVoice,
     PlayerEvents,
     PluginName,
     Cacher,
     Filter,
-  } = require(`@akarui/aoi.music`);
+  } = require(`@akarui/aoi.music`); // ajout du aoi.music
 
-require('./handler/status.js')(bot) //status handler
+require('./handler/status.js')(bot) //statut handler
 require('./handler/variables.js')(bot) //variables handler
 
 const loader = new aoijs.LoadCommands(bot)
-loader.load(bot.cmd,"./commands/")
+loader.load(bot.cmd,"./commands/")     // handler des commandes
 
-bot.readyCommand({
-    channel: "",
-    code: `$log[$userTag[$clientID] a redémarré avec succès :)]
-`
-})
 
+//<------Options de musique------>
 const voice = new AoiVoice(bot, {
     searchOptions: {
-        soundcloudClientId: "VTl9gNS05wF10zfiwKJ6FwK9mJsLVuAV",
+        soundcloudClientId: "ID SoundCloud à remplir !",
         youtubegl: "US",
     },
     requestOptions: {
@@ -37,7 +34,7 @@ voice.addPlugin( PluginName.Filter, new Filter( {
 }));
 voice.bindExecutor(bot.functionManager.interpreter);
 
-voice.addEvent("trackStart");
+voice.addEvent("trackStart");  //ajout des évents vocaux/musicaux
 voice.addEvent("trackEnd");
 voice.addEvent("queueEnd");
 voice.addEvent("queueStart");
@@ -45,4 +42,13 @@ voice.addEvent("audioError");
 voice.addEvent("trackPause");
 voice.addEvent("trackResume");
 
-loader.load(voice.cmds,"./voice/")
+loader.load(voice.cmds,"./voice/") //handler des commandes évents musique
+
+
+//<------Commandes dans l'index------>
+
+bot.readyCommand({ //commande de démarrage qui envoie un message dans la console, vous pouvez la custom pour qu'elle envoie aussi dans un salon discord
+    channel: "",
+    code: `$log[$userTag[$clientID] a redémarré avec succès :)] 
+`
+})
