56aa92d2ba911f5b9d4c9db052ab1cd8fe3121ea
s-ow <107432480+s-ow@users.noreply.github.com>
2023-02-23 23:40:11 +0100

    Create status.js

---
+++ handler/status.js
@@ -0,0 +1,9 @@
+module.exports = (bot) => {  //wiki aoi.js : https://aoi.js.org/docs/guides/status
+  
+    bot.status({
+        text: "s-ow qui travaille",
+        type: "WATCHING",
+      });
+  
+  
+    }
