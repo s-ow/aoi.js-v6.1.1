618c437041425f681a6ba948d87b6a3c0d115017
s-ow <107432480+s-ow@users.noreply.github.com>
2023-02-24 11:54:54 +0100

    Update trackEnd.js

--- voice/trackEnd.js
+++ voice/trackEnd.js
@@ -1,1 +1,10 @@
-
+// exemple d'utilisation avec "trackEnd", le but sera d'envoyer un message à chaque fin de morceau avec les infos du prochain morceau
+module.exports={
+  name:"trackEnd", // optionnel
+  type:"trackEnd", // obligatoire
+  channel:"$voiceID[$clientID]", //optionnel
+  code:`
+  $if[$queue!=;J'ai fini de jouer \`$songInfo[title;0]\`, je joue maintenant \`$songInfo[title;1]\` - __$humanizeMS[$songInfo[duration;1]]__ dans <#$voiceID[$clientID]>.;La playlist est terminée.]
+  `
+}
+  
