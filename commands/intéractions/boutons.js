2de89abe9b34318000f876ceff2ab04e30c2c018
s-ow <107432480+s-ow@users.noreply.github.com>
2023-02-24 15:03:03 +0100

    Update boutons.js

--- commands/intéractions/boutons.js
+++ commands/intéractions/boutons.js
@@ -1,1 +1,16 @@
-
+// dans ce fichier, nous allons créer une commande qui envoie un simple message suivi d'un bouton
+module.exports=[{
+  name:"bouton",
+  code:`
+  $addButton[1;Lien du wiki;primary;IDbouton;false;🔗]
+  Ce message est suivi d'un bouton.
+  `
+},{
+  name:"IDbouton",
+  type:"interaction",
+  prototype:"button",
+  code:`
+  $interactionReply[Le bouton fonctionne !;{newEmbed:{description:Voici le lien du [wiki sur les boutons](https://aoi.js.org/docs/functions/Calling/addButton/)}};;;everyone;true]
+  `
+}]
+// grâce au "true" à la fin, le message est même en ephemeral
